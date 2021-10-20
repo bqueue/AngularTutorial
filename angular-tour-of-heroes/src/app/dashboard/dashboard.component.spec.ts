4b6f04dcf51eb623a18d52c85cdb15634b1a323f
Brian <bquock@usc.edu>
2021-10-19 23:31:18 -0700

    Fix unit tests

--- angular-tour-of-heroes/src/app/dashboard/dashboard.component.spec.ts
+++ angular-tour-of-heroes/src/app/dashboard/dashboard.component.spec.ts
@@ -1,7 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-
+import { HeroSearchComponent } from '../hero-search/hero-search.component';
+import { MockComponent } from 'ng-mocks';
 import { DashboardComponent } from './dashboard.component';
+import { HeroService } from '../hero.service';
+import { HeroServiceMock } from '../../mocks/mocks';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
@@ -9,7 +12,16 @@ describe('DashboardComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ],
+      declarations: [ 
+        DashboardComponent,
+        MockComponent(HeroSearchComponent)
+      ],
+      providers: [
+        {
+          provide: HeroService, 
+          useValue: HeroServiceMock.instance()
+        }
+      ],
       imports: [RouterTestingModule]
     })
     .compileComponents();
